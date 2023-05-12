4c2aac1f3fa734e26bfb88161720ec31611d2eb6
changuii <rhljh7410@gmail.com>
2023-05-12 23:33:36 +0900

    commit

--- chapter5/jpa/src/main/java/dev/changui/jpa/TestComponent.java
+++ chapter5/jpa/src/main/java/dev/changui/jpa/TestComponent.java
@@ -1,35 +1,35 @@
-package dev.changui.jpa;
-
-
-import dev.changui.jpa.entity.BoardEntity;
-import dev.changui.jpa.entity.PostEntity;
-import dev.changui.jpa.repository.BoardRepository;
-import dev.changui.jpa.repository.PostRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-@Component
-public class TestComponent {
-    public TestComponent(
-            @Autowired BoardRepository boardRepository,
-            @Autowired PostRepository postRepository
-            ){
-        BoardEntity boardEntity = new BoardEntity();
-        boardEntity.setName("New board");
-        BoardEntity newBoardEntity = boardRepository.save(boardEntity);
-        System.out.println(newBoardEntity);
-
-        PostEntity postEntity = new PostEntity();
-        postEntity.setTitle("hello ORM");
-        postEntity.setContent("This Entity is created by hibernate!");
-        postEntity.setWriter("changui");
-        postEntity.setBoardEntity(newBoardEntity);
-        PostEntity newPostEntity = postRepository.save(postEntity);
-        System.out.println(newPostEntity);
-
-        System.out.println(postRepository.findAllByWriter("changui").size());
-
-
-    }
-
-}
+//package dev.changui.jpa;
+//
+//
+//import dev.changui.jpa.entity.BoardEntity;
+//import dev.changui.jpa.entity.PostEntity;
+//import dev.changui.jpa.repository.BoardRepository;
+//import dev.changui.jpa.repository.PostRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class TestComponent {
+//    public TestComponent(
+//            @Autowired BoardRepository boardRepository,
+//            @Autowired PostRepository postRepository
+//            ){
+//        BoardEntity boardEntity = new BoardEntity();
+//        boardEntity.setName("New board");
+//        BoardEntity newBoardEntity = boardRepository.save(boardEntity);
+//        System.out.println(newBoardEntity);
+//
+//        PostEntity postEntity = new PostEntity();
+//        postEntity.setTitle("hello ORM");
+//        postEntity.setContent("This Entity is created by hibernate!");
+//        postEntity.setWriter("changui");
+//        postEntity.setBoardEntity(newBoardEntity);
+//        PostEntity newPostEntity = postRepository.save(postEntity);
+//        System.out.println(newPostEntity);
+//
+//        System.out.println(postRepository.findAllByWriter("changui").size());
+//
+//
+//    }
+//
+//}
